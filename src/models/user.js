b2d59aa6bad1aef94ba3799f482b6a14392f9184
katpadi <katpadisaicharan@gmail.com>
2025-02-10 20:26:41 +0530

    JWT

--- src/models/user.js
+++ src/models/user.js
@@ -59,7 +59,7 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.getJWT = async function(){
     const user = this;
     const token = await jwt.sign({_id:user._id},"DEVTinder$740",{
-        expiresIn:"1d",
+        expiresIn:"7d",
     });
     return token;
 };
